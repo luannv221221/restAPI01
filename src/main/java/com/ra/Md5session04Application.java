22b3fedac7c7bc1d8faaa9af306ceaa64605b328
your new username <smallmonster900@gmail.com>
2023-12-12 10:16:50 +0700

    a

---
+++ src/main/java/com/ra/Md5session04Application.java
@@ -0,0 +1,13 @@
+package com.ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md5session04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md5session04Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/ra/entity/Category.java
@@ -0,0 +1,43 @@
+package com.ra.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String categoryName;
+    private Boolean categoryStatus;
+
+    public Category() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Boolean getCategoryStatus() {
+        return categoryStatus;
+    }
+
+    public void setCategoryStatus(Boolean categoryStatus) {
+        this.categoryStatus = categoryStatus;
+    }
+
+}
